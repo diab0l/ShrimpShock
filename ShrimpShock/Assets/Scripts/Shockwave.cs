fa9f6fc7654cedaf2f2730a7c59e66c678ac0b13
drominer <dreiner@zedat.fu-berlin.de>
2014-11-08 19:56:41 +0100

    Added basic mouse-click control
    The Player Object is now being shot in the direction of the mouse-cursor
    on clicking.

--- ShrimpShock/Assets/Scripts/Shockwave.cs
+++ ShrimpShock/Assets/Scripts/Shockwave.cs
@@ -5,10 +5,12 @@ public class Shockwave : MonoBehaviour {
 
     public Vector2 direction = new Vector2(10f, 10f);
     public float velocity;
+    public bool shootable;
 
 	// Use this for initialization
 	void Start () {
-        rigidbody2D.AddForce(direction*velocity);
+        //rigidbody2D.AddForce(direction*velocity);
+        shootable = true;
 	}
 	
 	// Update is called once per frame
@@ -16,8 +18,21 @@ public class Shockwave : MonoBehaviour {
         //gameObject.transform.localPosition += direction * velocity * Time.deltaTime;
         //Debug.Log("Vorwaerts");
         //rigidbody2D.velocity = rigidbody2D.velocity*0.9999f;
+        if (shootable)
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                direction = Camera.main.ScreenToWorldPoint(Input.mousePosition)-gameObject.transform.position;
+                rigidbody2D.AddForce(direction * velocity);
+            }
+            
+        }
+        if (rigidbody2D.velocity==(new Vector2(0.0f,0.0f)))
+        {
+            gameObject.transform.position = (new Vector2(0.0f, 0.0f));
+            shootable = true;
+        }
 	}
-
     void OnCollisionEnter2D(Collision2D col)
     {
     }
